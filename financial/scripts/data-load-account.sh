b75e85c04a2dbf7a0d2b93684c54e066691a56a3
DESKTOP-DKTP031\yvesl <yvesl@ngdata.com>
2017-11-21 12:26:41 +0100

    Change load from headers to columns

--- financial/scripts/data-load-account.sh
+++ financial/scripts/data-load-account.sh
@@ -2,5 +2,7 @@
 set -e;
 
 [[ -z "$1" ]] && echo "Usage: data-load-account.sh /path/to/file_to_load.csv" && exit 1;
-headers=$(cat ${1/.csv/.hdr} | tr ';' ',')
-lily csv-bulk-loader -i file://$1 -t account -s 2014-01-01T00:00:00.000Z -d ';' -c ${headers}
+#headers=$(cat ${1/.csv/.hdr} | tr ';' ',')
+#lily csv-bulk-loader -i file://$1 -t account -s 2014-01-01T00:00:00.000Z -d ';' -c ${headers}
+lily csv-bulk-loader -i file://$1  -t account -d ';' -s 2014-01-01T00:00:00.000Z -c account_id,category,sub_category,balance,active;
+
