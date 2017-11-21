5cd5101178aa5ef3eff21db3e6f7d4753dec5470
DESKTOP-DKTP031\yvesl <yvesl@ngdata.com>
2017-11-21 12:29:27 +0100

    Change load from headers to columns

--- financial/scripts/data-load-account_role.sh
+++ financial/scripts/data-load-account_role.sh
@@ -2,5 +2,6 @@
 set -e;
 
 [[ -z "$1" ]] && echo "Usage: data-load-account_role.sh /path/to/file_to_load.csv" && exit 1;
-headers=$(cat ${1/.csv/.hdr} | tr ';' ',')
-lily csv-bulk-loader -i file://$1 -t account_role -s 2014-01-01T00:00:00.000Z -d ';' -c ${headers}
+#headers=$(cat ${1/.csv/.hdr} | tr ';' ',')
+#lily csv-bulk-loader -i file://$1 -t account_role -s 2014-01-01T00:00:00.000Z -d ';' -c ${headers}
+lily csv-bulk-loader -i file://$1  -t account_role -d ';' -s 2014-01-01T00:00:00.000Z -c customer_id,account_id,role;
